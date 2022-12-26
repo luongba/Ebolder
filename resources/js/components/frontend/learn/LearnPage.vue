91dd28bc72f9e827340374bf35ff48a339d1e142
luongba <a2k10lnc@gmail.com>
2022-12-26 07:12:10 +0700

    aaaaa

--- resources/js/components/frontend/learn/LearnPage.vue
+++ resources/js/components/frontend/learn/LearnPage.vue
@@ -1,51 +1,51 @@
 <template>
-    <div class="wrapper">
-        <div id="background-wrap">
-            <div class="x1">
-                <div class="cloud"></div>
-            </div>
+  <div class="wrapper">
+    <div id="background-wrap">
+      <div class="x1">
+        <div class="cloud"></div>
+      </div>
 
-            <div class="x2">
-                <div class="cloud"></div>
-            </div>
+      <div class="x2">
+        <div class="cloud"></div>
+      </div>
 
-            <div class="x3">
-                <div class="cloud"></div>
-            </div>
+      <div class="x3">
+        <div class="cloud"></div>
+      </div>
 
-            <div class="x4">
-                <div class="cloud"></div>
-            </div>
+      <div class="x4">
+        <div class="cloud"></div>
+      </div>
 
-            <div class="x5">
-                <div class="cloud"></div>
+      <div class="x5">
+        <div class="cloud"></div>
+      </div>
+      <div class="cloud-box" id="cloud-box">
+        <div class="box" id="cloud-3">
+          <div class="box-inner">
+            <div class="box-filter"></div>
+            <div class="box-content">
+              <h2 class="title">Level 3</h2>
             </div>
-            <div class="cloud-box" id="cloud-box">
-                <div class="box" id="cloud-3">
-                    <div class="box-inner">
-                        <div class="box-filter"></div>
-                        <div class="box-content">
-                            <h2 class="title">Level 3</h2>
-                        </div>
-                    </div>
-                </div>
-                <div class="box" id="cloud-2">
-                    <div class="box-inner">
-                        <div class="box-filter"></div>
-                        <div class="box-content">
-                            <h2 class="title">Level 2</h2>
-                        </div>
-                    </div>
-                </div>
-                <div class="box" id="cloud-1">
-                    <div class="box-inner">
-                        <div class="box-filter"></div>
-                        <div class="box-content">
-                            <h2 class="title">Level 1</h2>
-                        </div>
-                    </div>
-                </div>
-                <!-- <div class="big-cloud">
+          </div>
+        </div>
+        <div class="box" id="cloud-2">
+          <div class="box-inner">
+            <div class="box-filter"></div>
+            <div class="box-content">
+              <h2 class="title">Level 2</h2>
+            </div>
+          </div>
+        </div>
+        <div class="box" id="cloud-1">
+          <div class="box-inner">
+            <div class="box-filter"></div>
+            <div class="box-content">
+              <h2 class="title">Level 1</h2>
+            </div>
+          </div>
+        </div>
+        <!-- <div class="big-cloud">
                     <h2 class="title" id="cloud-3">Level 3</h2>
                     <div class="clound-children"></div>
                 </div>
@@ -57,81 +57,79 @@
                     <h2 class="title">Level 1</h2>
                     <div class="clound-children"></div>
                 </div> -->
-            </div>
-        </div>
-        <div class="absolute bottom-[10%] right-[5%] z-10">
-            <div class="mb-4">
-                <el-button
-                    icon="el-icon-arrow-up"
-                    circle
-                    @click="cloudUp"
-                ></el-button>
-            </div>
-            <div>
-                <el-button
-                    icon="el-icon-arrow-down"
-                    circle
-                    @click="cloudDown"
-                ></el-button>
-            </div>
-        </div>
+      </div>
     </div>
+    <div class="absolute bottom-[10%] right-[5%] z-10">
+      <div class="mb-4">
+        <el-button icon="el-icon-arrow-up" circle @click="cloudUp"></el-button>
+      </div>
+      <div>
+        <el-button
+          icon="el-icon-arrow-down"
+          circle
+          @click="cloudDown"
+        ></el-button>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 import "./learn.css";
 export default {
-    data() {
-        return {
-            level: 1,
-        };
+  data() {
+    return {
+      level: 1,
+    };
+  },
+  methods: {
+    cloudUp() {
+      this.level++;
+      this.scrollToCloud();
     },
-    methods: {
-        cloudUp() {
-            this.level++;
-            this.scrollToCloud();
-        },
-        cloudDown() {
-            this.level--;
-            this.scrollToCloud();
-        },
-        scrollToCloud() {
-            let cloud = document.getElementById(`cloud-${this.level}`);
-            cloud.scrollIntoView({
-                behavior: "smooth",
-                block: "center",
-                inline: "center",
-            });
-        },
+    cloudDown() {
+      this.level--;
+      this.scrollToCloud();
     },
-    mounted() {
-        setTimeout(() => {
-            this.scrollToCloud();
-        }, 1000);
+    scrollToCloud() {
+      let cloud = document.getElementById(`cloud-${this.level}`);
+      cloud.scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+        inline: "center",
+      });
     },
+  },
+  mounted() {
+    setTimeout(() => {
+      this.scrollToCloud();
+    }, 1000);
+  },
 };
 </script>
 <style scoped>
 .title {
-    font-size: 40px;
-    text-align: center;
-    z-index: 3;
-    color: #fff;
+  font-size: 40px;
+  text-align: center;
+  z-index: 3;
+  color: #fff;
 }
 ::-webkit-scrollbar {
-    display: none;
+  display: none;
 }
 .box-content {
-    width: 100%;
-    position: relative;
-    padding: 40px;
-    z-index: 6;
-    background: rgba(240, 128, 0,0.6);
+  width: 100%;
+  position: relative;
+  padding: 40px;
+  z-index: 6;
+  background: rgba(240, 128, 0, 0.6);
+
+  box-sizing: border-box;
 }
 .box-filter {
-    position: absolute;
-    inset: 0;
-    filter: blur(8px);
-    -webkit-filter: blur(8px);
-    z-index: 5;
+  position: absolute;
+  inset: 0;
+  filter: blur(8px);
+  -webkit-filter: blur(8px);
+  z-index: 5;
 }
 </style>
