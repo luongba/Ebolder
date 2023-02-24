2d8758f9b99d52bc28b1fbb83ffc9d2073f290d6
luongba <53484184+luongba@users.noreply.github.com>
2023-02-24 08:27:24 +0700

    Update Footer.vue
    Thay đổi footer

--- resources/js/components/frontend/Footer.vue
+++ resources/js/components/frontend/Footer.vue
@@ -4,11 +4,11 @@
             <ul>
                 <li class="text-[20px] mb-2">
                     <span><i class="el-icon-phone"></i></span>
-                    <span class="text-[16px]">01234.567.89</span>
+                    <span class="text-[16px]">0961.545.114</span>
                 </li>
                 <li class="text-[20px] mb-2">
                     <span><i class="el-icon-message"></i></span>
-                    <span class="text-[16px]">contact@english.edu.vn</span>
+                    <span class="text-[16px]">Email: DTC18H4801030031@ictu.edu.vn</span>
                 </li>
                 <li class="text-[20px] mb-2">
                     <span><i class="el-icon-place"></i></span>
@@ -17,31 +17,19 @@
             </ul>
             <ul>
                 <li class="text-[20px] mb-2 uppercase strong font-semibold">Hỗ trợ</li>
-                <li class="text-[20px] mb-2">
-                    <span class="text-[16px]">Liên hệ</span>
-                </li>
-                <li class="text-[20px] mb-2">
-                    <span class="text-[16px]">Bảo mật</span>
-                </li>
             </ul>
             <ul>
                 <li class="text-[20px] mb-2 uppercase strong font-semibold">
                     VỀ chúng tôi
                 </li>
-                <li class="text-[20px] mb-2">
-                    <span class="text-[16px]">Liên hệ</span>
-                </li>
-                <li class="text-[20px] mb-2">
-                    <span class="text-[16px]">Bảo mật</span>
-                </li>
             </ul>
             <ul>
                 <li class="text-[20px] mb-2 uppercase strong font-semibold">Hỗ trợ</li>
                 <li class="text-[20px] mb-2">
-                    <span class="text-[16px]">Liên hệ</span>
+                    <span class="text-[16px]">Liên hệ: 0961.545.114</span>
                 </li>
                 <li class="text-[20px] mb-2">
-                    <span class="text-[16px]">Bảo mật</span>
+                    <span class="text-[16px]">Email: DTC18H4801030031@ictu.edu.vn</span>
                 </li>
             </ul>
         </div>
